0f1f8c81fd07a83bd5ce7a3f178ef4bc1c645a0e
ederray <eder.ray@gmail.com>
2025-11-25 22:31:01 +0000

    feat/ingestao-bronze: inserção das features finais na camada bronze e ajustes de funções

--- src/features/utils.py
+++ src/features/utils.py
@@ -4,6 +4,8 @@ from datetime import date
 from io import StringIO
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+import urllib3 
 
 spark = SparkSession.builder.appName("API_Forecasting_Utils").getOrCreate()
 
@@ -55,7 +57,7 @@ def salvar_tabela_delta_spark(df, path: str, partitionby:list, mode:str="overwri
     print('Tabela salva com sucesso')
 
 
-def cotacao_atual_html(url:str, cols_sanitizacao:dict) -> DataFrame:
+def cotacao_atual_etanol_html(url:str, cols_sanitizacao:dict) -> DataFrame:
 
     headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
@@ -91,5 +93,56 @@ def cotacao_atual_html(url:str, cols_sanitizacao:dict) -> DataFrame:
     df_spark = spark.createDataFrame(cotacao_atual)
 
     return df_spark
+
+def cotacao_gasolina_site_petrobras(URL):
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    response = requests.get(URL, headers=headers, timeout=10)
+    
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    PRECO_ATUAL = soup.find(id='preçomedioBrasil').text
+
+    datas_coleta = soup.find("span", {"data-lfr-editable-id": "telafinal-textoColeta"}).get_text()
+
+    datas_coleta = datas_coleta.split()
+    DATA_INICIAL = datas_coleta[0]
+    DATA_FINAL = datas_coleta[2]
+
+    return PRECO_ATUAL, DATA_INICIAL, DATA_FINAL
+
+
+def volume_acucar_exportacao(mes_ano_inicio:str, mes_ano_fim:str):
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    URL = "https://api-comexstat.mdic.gov.br/general?language=pt"
+    payload = {
+        "flow": "export",
+        "monthDetail": True,
+        "period": {"from": mes_ano_inicio, "to": mes_ano_fim},
+        "filters": [
+            {"filter": "ncm", "values": ["17011300"]}
+        ],
+        "details": ["ncm"],
+        "metrics": ["metricKG"]
+    }
+
+    response = requests.post(URL, json=payload, verify=False)
+    response.raise_for_status()
+
+    data = response.json()["data"]["list"]
+    df = pd.DataFrame(data)
+
+    df = df.sort_values(["year", "monthNumber"]).reset_index(drop=True)
+
+    df["date"] = pd.to_datetime(df["year"].astype(str) + "-" +
+                                df["monthNumber"].astype(str) + "-01")
+
+    df = df[["date", "year", "monthNumber", "metricKG", "ncm"]]
+
+    return df
+
+    
 
 
